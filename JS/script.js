d3aa68980a98c6e201b32e683aa0f7903489d586
Kira-Marie Lazda <lazda.kira@gmail.com>
2021-12-05 10:12:57 -0800

    kl

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 //Last Updated: 11/7/2021 by Kira
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("navbar");
+    var x = document.getElementById("myTopnav");
     if (x.className === "NavigationBar") {
       x.className += " responsive";
     } else {
